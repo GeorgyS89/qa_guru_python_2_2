734dc3e2eb4e07e60ae01592fd54fffe66ba360a
GeorgyS89 <mr.hazzzard@gmail.com>
2022-08-31 16:07:51 +0300

    created a negative and a positive test

---
+++ test_2_2.py
@@ -0,0 +1,14 @@
+
+from selene.support.shared import browser
+from selene import be, have
+
+def test_google(browser_size):
+    browser.element('[name="q"]').should(be.blank).type('selene python').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+def test_negative_google(browser_size):
+    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
+    browser.element('[id="search"]').should(have.no.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+
